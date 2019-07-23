359db2e15239ebb01d15014d4c78d27c2584487f
gadang23 <hasea23@gmail.com>
2019-07-23 22:32:49 +0700

    2

--- mysql.go
+++ mysql.go
@@ -1,11 +1,11 @@
-package main
+package Mysql
 
 import (
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func main() {
+func mysql() {
 	db, err := sql.Open("mysql", "bagas:123@localhost:3306/octopus")
 	if err != nil {
 		panic(err.Error())  // Just for example purpose. You should use proper error handling instead of panic
